c9d7bc5afaab51d747c36ff7a9e40d6c64baccda
izen1231 <izen1231@gmail.com>
2019-06-19 08:35:07 +0900

    update

--- app/src/main/java/com/example/ontime/NetRetrofit.java
+++ app/src/main/java/com/example/ontime/NetRetrofit.java
@@ -1,16 +1,10 @@
 package com.example.ontime;
 
-import com.google.gson.JsonObject;
-
-import java.util.ArrayList;
-
-import okhttp3.OkHttpClient;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
-import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 interface RetrofitService {
